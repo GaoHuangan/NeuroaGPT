6328a823135b0783894b7901ba9b2b4a5d4f94fb
gaohuangan <gaohuangan@yeah.net>
2025-09-02 23:23:12 +0900

    feat:fix appContext can't shale data,and complete Conponent of the chatBox and Message   v2_20250902_2323

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import moment from 'moment'
+import Markdown from 'react-markdown'
+import { useEffect } from 'react'
+import Prism from 'prismjs'
 
-const Message = () => {
+const Message = ({ message }) => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [message.content]);
   return (
-    <div>Message</div>
+    <div>
+      {message.role === "user" ? (
+        <div className='flex items-start justify-end my-4 gap-2'>
+          <div className='flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl'>
+            <p className='text-sm dark:text-primary dark:invert'>{message.content}</p>
+            <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{moment(message.timestamp).fromNow()}</span>
+          </div>
+          <img src={assets.user_icon} className='w-8 rounded-full' alt="" />
+        </div>
+      ) : (
+        <div className='inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-primary/20 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md my-4'>
+          {message.isImage ? (
+            <img src={message.content} alt="" className='w-full max-w-md mt-2 rounded-md' />
+          ) : (
+            <div className='text-sm dark:text-primary reset-tw dark:invert'><Markdown>{message.content}</Markdown></div>
+          )}
+          <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{moment(message.timestamp).fromNow()}</span>
+        </div>
+      )}
+    </div>
   )
 }
 
--- client/src/components/SiderBar.jsx
+++ client/src/components/SiderBar.jsx
@@ -3,17 +3,17 @@ import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import moment from 'moment'
 
-const SiderBar = () => {
+const SiderBar = ({ isMenuOpen, setIsMenuOpen }) => {
   // Extract necessary state and functions from the app context
   const { chats, setSelectChat, theme, setTheme, user, navigate } = useAppContext();
   // Local state for search functionality
   const [search, setSearch] = useState('');
 
   return (
-    <div className={`flex flex-col h-screen min-w-72 p-5 transition-all duration-500 max-md:absolute left-0 z-10 ${theme === 'dark'
-      ? 'bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl'
-      : 'bg-gradient-to-b from-white/80 to-gray-50/80 border-r border-gray-200/50 backdrop-blur-3xl'
-      }`}>
+    <div className={`flex flex-col h-screen w-80 p-5 transition-all duration-500 max-md:fixed max-md:top-0 max-md:left-0 max-md:z-50 ${theme === 'dark'
+      ? 'bg-[#242124]/95 backdrop-blur-xl border-r border-[#80609F]/50'
+      : 'bg-white/95 backdrop-blur-xl border-r border-gray-300'
+      } ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
       {/* LOGO SECTION */}
       {/* Display different logo based on current theme (dark/light) */}
       <img
@@ -57,7 +57,7 @@ const SiderBar = () => {
               : chat.name.toLowerCase().includes(search.toLowerCase())
           ).map((chat) => (
             // Individual chat item
-            <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
+            <div onClick={() => { navigate('/'); setSelectChat(chat); setIsMenuOpen(false) }} key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
               {/* CHAT CONTENT */}
               <div>
                 {/* Display first message preview or chat name (truncated to 32 characters) */}
@@ -81,14 +81,14 @@ const SiderBar = () => {
         }
       </div>
       {/* Community Image */}
-      <div onClick={() => { navigate('/community') }} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+      <div onClick={() => { navigate('/community'); setIsMenuOpen(false) }} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
         <img src={assets.gallery_icon} className='w-4.5 not-dark:invert' alt="" />
         <div className='flex flex-col text-sm'>
           <p>Community Images</p>
         </div>
       </div>
       {/* Credit Purchase Option */}
-      <div onClick={() => { navigate('/credits') }} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+      <div onClick={() => { navigate('/credits'); setIsMenuOpen(false) }} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
         <img src={assets.diamond_icon} className='w-4.5 dark:invert' alt="" />
         <div className='flex flex-col text-sm'>
           <p>Credits:{user?.credits}</p>
@@ -109,6 +109,14 @@ const SiderBar = () => {
           <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4'></span>
         </label>
       </div>
+
+      {/* User Account */}
+      <div className='flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group'>
+        <img src={assets.user_icon} className='w-7 rounded-full' alt="" />
+        <p className='flex-1 text-sm dark:text-primary truncate'>{user ? user.name : "Login to your account"}</p>
+        {user && <img src={assets.logout_icon} className='h-5 cursor-pointer hidden not-dark:invert group-hover:block' alt="" />}
+      </div>
+      <img onClick={() => setIsMenuOpen(false)} src={assets.close_icon} className='absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden dark:invert' alt="" />
     </div>
   )
 }
